ef2dd093e3a8a0ff466d8c0f9eea8e83c303cbdd
Evan Dickinson <evan_dickinson@protonmail.com>
2018-11-14 14:40:16 -0800

    init

---
+++ kLua/src/kLuaMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using MoonSharp.Interpreter;
+
+namespace kLua
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class kLuaMain : MonoBehaviour
+    {
+        public void Start(){
+
+        }
+
+        public void Update()
+        {
+            Debug.Log(Script.VERSION);
+        }
+    }
+}
