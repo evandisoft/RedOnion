8d321174008b7571db5c96b57c039599ab8d554f
Evan Dickinson <evan_dickinson@protonmail.com>
2019-03-02 15:48:47 -0800

    My attempt to translate the tests into nunit

---
+++ RedOnion.ScriptNUnit/OpCodeTests.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using RedOnion.Script.Execution;
+
+namespace RedOnion.ScriptTests
+{
+	[TestFixture]
+	public class OpCodeTests
+	{
+		[Test]
+		public void OpCode_01_BasicExtend()
+		{
+			Assert.AreEqual(OpCode.Undefined, (OpCode)0);
+			Assert.AreEqual(OpCode.Undefined, ((OpCode)0).Extend());
+			Assert.AreEqual(OpCode.Byte, ((OpCode)OpCode.Byte.Code()).Extend());
+			Assert.AreEqual(OpCode.Double, ((OpCode)OpCode.Double.Code()).Extend());
+			Assert.AreEqual(OpCode.Create, ((OpCode)OpCode.Create.Code()).Extend());
+			Assert.AreEqual(OpCode.Assign, ((OpCode)OpCode.Assign.Code()).Extend());
+			Assert.AreEqual(OpCode.BitOr, ((OpCode)OpCode.BitOr.Code()).Extend());
+			Assert.AreEqual(OpCode.BitXor, ((OpCode)OpCode.BitXor.Code()).Extend());
+			Assert.AreEqual(OpCode.BitAnd, ((OpCode)OpCode.BitAnd.Code()).Extend());
+			Assert.AreEqual(OpCode.Add, ((OpCode)OpCode.Add.Code()).Extend());
+			Assert.AreEqual(OpCode.Mul, ((OpCode)OpCode.Mul.Code()).Extend());
+			Assert.AreEqual(OpCode.LogicOr, ((OpCode)OpCode.LogicOr.Code()).Extend());
+			Assert.AreEqual(OpCode.LogicAnd, ((OpCode)OpCode.LogicAnd.Code()).Extend());
+			Assert.AreEqual(OpCode.Plus, ((OpCode)OpCode.Plus.Code()).Extend());
+			Assert.AreEqual(OpCode.Neg, ((OpCode)OpCode.Neg.Code()).Extend());
+			Assert.AreEqual(OpCode.PostInc, ((OpCode)OpCode.PostInc.Code()).Extend());
+		}
+
+		[Test]
+		public void OpCode_02_BasicText()
+		{
+			Assert.AreEqual("undefined", OpCode.Undefined.Text());
+			Assert.AreEqual("=", OpCode.Assign.Text());
+			Assert.AreEqual("*", OpCode.Mul.Text());
+			Assert.AreEqual("+", OpCode.Plus.Text());
+			Assert.AreEqual("for", OpCode.For.Text());
+			Assert.AreEqual("if", OpCode.If.Text());
+			Assert.AreEqual("else", OpCode.Else.Text());
+			Assert.AreEqual("public", OpCode.Public.Text());
+			Assert.AreEqual("virtual", OpCode.Virtual.Text());
+			Assert.AreEqual("class", OpCode.Class.Text());
+			Assert.AreEqual("interface", OpCode.Interface.Text());
+			Assert.Null(((OpCode)0xFFFF).Text());
+		}
+
+		[Test]
+		public void OpCode_03_Priority()
+		{
+			Assert.True(OpCode.Mul.Priority() > OpCode.Add.Priority());
+			Assert.True(OpCode.Sub.Priority() > OpCode.ShiftRight.Priority());
+			Assert.True(OpCode.As.Priority() > OpCode.Equals.Priority());
+			Assert.True(OpCode.LogicAnd.Priority() > OpCode.LogicOr.Priority());
+			Assert.True(OpCode.Less.Priority() > OpCode.Differ.Priority());
+			Assert.True(OpCode.BitOr.Priority() > OpCode.Equals.Priority());
+			Assert.True(OpCode.BitOr.StdPriority() < OpCode.Equals.StdPriority());
+			Assert.True(OpCode.BitOr.Priority() < OpCode.ShiftLeft.Priority());
+			Assert.True(OpCode.BitOr.StdPriority() < OpCode.ShiftLeft.StdPriority());
+		}
+
+		[Test]
+		public void OpCode_04_TableTest()
+		{
+			var flags = true;
+			var prev = OpCode.Undefined;
+			foreach (FieldInfo fi in typeof(OpCode).GetFields().OrderBy(x => x.MetadataToken))
+			{
+				if (!fi.IsStatic)
+					continue;
+				var op = (OpCode)fi.GetValue(null);
+				if (flags)
+				{
+					if (op != OpCode.Undefined)
+						continue;
+					flags = false;
+				}
+				if ((byte)op != (byte)prev)
+				{
+					Assert.AreEqual(op.Extend(), op);
+					if ((byte)op >= 0x80)
+						Assert.True((byte)op == (ushort)op);
+				}
+				prev = op;
+			}
+		}
+	}
+}
---
+++ RedOnion.ScriptNUnit/ScannerTests.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+using RedOnion.Script.Parsing;
+
+namespace RedOnion.ScriptTests
+{
+	[TestFixture]
+	public class ScannerTests: Scanner
+	{
+		public void Test(string line)
+			=> Line = line;
+
+		[Test]
+		public void Scanner_01_Words()
+		{
+			Test("word");
+			Assert.AreEqual("word", Word);
+			Assert.AreEqual(0, At);
+			Assert.AreEqual(4, End);
+			Assert.AreEqual('w', CharAt(0));
+			Assert.AreEqual('d', CharAt(3));
+			Assert.True(White);
+			Assert.True(PeekEol);
+			Assert.AreEqual('\n', PeekAt(0));
+			Assert.AreEqual('\0', PeekAt(1));
+
+			Test("$small_under");
+			Assert.AreEqual("$small_under", Word);
+
+			Test("first second");
+			Assert.AreEqual(Word, "first");
+			Assert.AreEqual(' ', Peek);
+			Assert.AreEqual('s', PeekAt(1));
+			Assert.True(White);
+			Assert.AreEqual(NextWord(), "second");
+			Assert.True(White);
+		}
+
+		[Test]
+		public void Scanner_02_String()
+		{
+			Test("\"string\"");
+			Assert.AreEqual('"', Curr);
+			Assert.AreEqual(0, At);
+			Assert.AreEqual(8, End);
+			Assert.AreEqual('"', Read());
+			Assert.AreEqual("string\"", Rest());
+			Assert.True(Normal);
+
+			Test("@\"verbatim\"");
+			Assert.AreEqual("@\"verbatim\"", Rest());
+			Assert.True(Normal);
+
+			Test("$\"inter\"");
+			Assert.AreEqual("$\"inter\"", Rest());
+			Assert.True(Normal);
+
+			Test("$@\"inter-verbatim\"");
+			Assert.AreEqual("$@\"inter-verbatim\"", Rest());
+			Assert.True(Normal);
+
+			Test("\"esc\\\"\"");
+			Assert.AreEqual("\"esc\\\"\"", Rest());
+			Assert.True(Normal);
+
+			Test("@\"esc\\\"");
+			Assert.AreEqual("@\"esc\\\"", Rest());
+			Assert.True(Normal);
+
+			Test("\"blah");
+			Assert.True(InString);
+			Assert.AreEqual(5, End);
+			Line = "end\"";
+			Assert.True(Normal);
+			Assert.AreEqual(4, End);
+		}
+
+		[Test]
+		public void Scanner_03_Number()
+		{
+			Test("12345");
+			Assert.AreEqual('1', Curr);
+			Assert.AreEqual('1', Read());
+			Assert.AreEqual('2', Read());
+			Assert.AreEqual("345", Rest());
+
+			Test("0x1AB");
+			Assert.AreEqual('0', Read());
+			Assert.AreEqual('x', Read());
+			Assert.AreEqual("1AB", Rest());
+
+			Test("1.2");
+			Assert.AreEqual("1.2", Rest());
+
+			Test("1.2e+3");
+			Assert.AreEqual("1.2e+3", Rest());
+		}
+
+		[Test]
+		public void Scanner_04_Comments()
+		{
+			Test("first /* comment */ second");
+			Assert.AreEqual(Word, "first");
+			Assert.AreEqual(NextWord(), "second");
+
+			Test("/*");
+			Assert.True(Comment);
+
+			Test("comment");
+			Assert.True(Comment);
+
+			Test("*/");
+			Assert.True(Normal);
+		}
+	}
+}
