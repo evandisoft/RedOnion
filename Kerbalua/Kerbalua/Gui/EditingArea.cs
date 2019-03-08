df04245c48cae898f2d7cf7426335c75e214f6d1
Evan Dickinson <evan_dickinson@protonmail.com>
2019-03-07 21:27:21 -0800

    Second attempt

--- Kerbalua/Kerbalua/Gui/EditingArea.cs
+++ Kerbalua/Kerbalua/Gui/EditingArea.cs
@@ -64,6 +64,7 @@ namespace Kerbalua.Gui {
 			//}
 
 			if (event1.type == EventType.KeyDown) {
+				GUIUtil.ConsumeMarkedCharEvent(event1);
 				switch (event1.keyCode) {
 				case KeyCode.Tab:
 					//Debug.Log(event1.keyCode);
@@ -216,7 +217,7 @@ namespace Kerbalua.Gui {
 					Debug.Log(event1 + " was not used");
 				}
 
-				GUIUtil.ConsumeMarkedCharEvent(event1);
+
 			}
 		}
 
