cf599448093ee666c6993aaee93d320c2817f652
Evan Dickinson <evan_dickinson@protonmail.com>
2019-07-17 16:23:42 -0700

    Added "autorun" to api.
    Allows one to autorun.scripts to get a list of the autorun
    scripts. autorun.save(scriptnamess) to save a new list.
    autorun.add(scriptname) to add a new scriptname (will save it)

--- RedOnion.KSP/API/AutoRun.cs
+++ RedOnion.KSP/API/AutoRun.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using RedOnion.KSP.Settings;
 
 namespace RedOnion.KSP.API
 {
 	public class AutoRun
 	{
+		const string AutoRunSettingName = "AutoRun";
 
 		public AutoRun()
 		{
@@ -12,6 +14,24 @@ namespace RedOnion.KSP.API
 
 		public static AutoRun Instance = new AutoRun();
 
-		//public IList<string> scripts => GlobalSettings
+
+#pragma warning disable IDE1006 // Naming Styles
+
+		public IList<string> scripts() => SavedSettings.LoadListSetting(AutoRunSettingName);
+
+
+		public void save(IList<string> scripts)
+		{
+			SavedSettings.SaveListSetting(AutoRunSettingName, scripts);
+		}
+
+		public void add(string scriptname)
+		{
+			var s = scripts();
+			s.Add(scriptname);
+			save(s);
+		}
+
+#pragma warning restore IDE1006 // Naming Styles
 	}
 }
