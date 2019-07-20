e53986c80aceb463f44c5f40b9f7384b6ed6476d
Evan Dickinson <evan_dickinson@protonmail.com>
2019-07-20 00:35:19 -0700

    Saving for Refactoring.

---
+++ RedOnion.KSP/OS/ROOS.cs
@@ -0,0 +1,18 @@
+using System;
+namespace RedOnion.KSP.OS
+{
+	/// <summary>
+	/// RedOnionOperatingSystem
+	/// 
+	/// This is meant to take script execution management out of ScriptWindow.
+	/// </summary>
+	public class ROOS
+	{
+		public ROOS()
+		{
+
+		}
+
+
+	}
+}
