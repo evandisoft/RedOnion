0d347a16f42eab66f528394e302964ce3c421c01
evandisoft <evan_dickinson@protonmail.com>
2019-11-21 01:23:53 -0800

    majorMalfunction.lua rewritten.
    Had to use "native" explode

--- GameData/RedOnion/Scripts/majorMalfunction.lua
+++ GameData/RedOnion/Scripts/majorMalfunction.lua
@@ -1,13 +1,12 @@
-vessel=import.FlightGlobals.ActiveVessel
-
-Random=import.UnityEngine.Random
+Random=native.UnityEngine.Random
 
 sleep(10)
 
 for j=1,8 do
+    sleep(Random.Range(0,10))
     
-    local num=Random.Range(0,vessel.parts.Count)
-    print(vessel.parts[num].ToString().."is malfunctioning!")
-    vessel.parts[num].explode()
+    local num=Random.Range(0,ship.parts.count)
+    print(ship.parts[num].ToString().."is malfunctioning!")
+    ship.parts[num].native.explode()
 end
 print("Done!")
