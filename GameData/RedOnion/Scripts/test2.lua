c6128209b8dd94d51fa7839053e9954abe2ce67c
evandisoft <evan_dickinson@protonmail.com>
2019-12-25 06:48:55 -0800

    script change

--- GameData/RedOnion/Scripts/test2.lua
+++ GameData/RedOnion/Scripts/test2.lua
@@ -1,19 +1,8 @@
-function inf()
+function printlots()
     for i=1,10000000 do
         print(i)
     end
-    --error("blah")
 end
 
-function inf2()
-    for i=1,100 do
-        pcall(inf)
-        print("after error")
-        --sleep(1)
-    end
-    print("all done")
-end
 
-pcall(inf2)
-
-print("past")
+printlots()
