909eb4bcefc38045b80a159539cd31d1a5709c1d
firdacz <firdacz@gmail.com>
2020-02-21 21:31:38 +0100

    part-values enhanced

--- RedOnion.KSP/Parts/PartValues.cs
+++ RedOnion.KSP/Parts/PartValues.cs
@@ -1,5 +1,6 @@
 using RedOnion.Collections;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -49,7 +50,7 @@ namespace RedOnion.KSP.Parts
 				GameEvents.onPartDestroyed.Add(CheckPart);
 				GameEvents.onPartDie.Add(CheckPart);
 			}
-			public void Update(string text)
+			public void SetText(string text)
 				=> box.Text = text;
 			void Closed(UI.Window window)
 			{
@@ -81,9 +82,9 @@ namespace RedOnion.KSP.Parts
 			}
 		}
 
-		ListCore<KeyValuePair<string,string>> list;
-		bool parsed;
-		void SetValues(string values)
+		internal ListCore<KeyValuePair<string,string>> list;
+		internal bool parsed;
+		internal void SetValues(string values)
 		{
 			if (values == null)
 				values = "";
@@ -91,10 +92,11 @@ namespace RedOnion.KSP.Parts
 				return;
 			this.values = values;
 			list.Clear();
-			Parse();
-			Update();
+			ParseValues();
+			UpdateList();
 		}
-		void Update()
+		//NOTE: don't even think about renaming this to Update! Unity!!
+		internal void UpdateList()
 		{
 			var sb = new StringBuilder();
 			foreach (var pair in list)
@@ -106,9 +108,9 @@ namespace RedOnion.KSP.Parts
 				else sb.Append(pair.Key).Append('=').Append(pair.Value);
 			}
 			values = sb.ToString();
-			editor?.Update(values);
+			editor?.SetText(values);
 		}
-		void Parse()
+		internal void ParseValues()
 		{
 			parsed = true;
 			var full = values;
@@ -138,7 +140,7 @@ namespace RedOnion.KSP.Parts
 					if (at != start)
 					{
 						var name = full.Substring(start, at-start);
-						list.Add(new KeyValuePair<string, string>(name, null));
+						list.Add(new KeyValuePair<string, string>(name, ""));
 					}
 					start = ++at;
 					continue;
@@ -146,61 +148,109 @@ namespace RedOnion.KSP.Parts
 				at++;
 			}
 		}
-		public bool has(string tag)
+		public int count
+		{
+			get
+			{
+				if (!parsed)
+					ParseValues();
+				return list.size;
+			}
+		}
+		public int indexOf(string tag)
 		{
 			if (!parsed)
-				Parse();
-			foreach (var pair in list)
-				if (pair.Key.Equals(tag, StringComparison.OrdinalIgnoreCase))
-					return true;
-			return false;
+				ParseValues();
+			for (int i = 0, n = list.size; i < n; i++)
+				if (list.items[i].Key.Equals(tag, StringComparison.OrdinalIgnoreCase))
+					return i;
+			return -1;
 		}
+		public bool has(string tag)
+			=> indexOf(tag) >= 0;
 		public string get(string tag)
 		{
-			if (!parsed)
-				Parse();
-			foreach (var pair in list)
-				if (pair.Key.Equals(tag, StringComparison.OrdinalIgnoreCase))
-					return pair.Value;
-			return null;
+			var at = indexOf(tag);
+			return at < 0 ? null : list.items[at].Value;
 		}
-		public void set(string tag, string value = null)
+		public void set(string tag, string value = "")
 		{
-			if (!parsed)
-				Parse();
-			for (int i = 0; i < list.size; i++)
+			var at = indexOf(tag);
+			if (at >= 0)
 			{
-				ref var pair = ref list.items[i];
-				if (!pair.Key.Equals(tag, StringComparison.OrdinalIgnoreCase))
-					continue;
-				pair = new KeyValuePair<string, string>(tag, value);
-				Update();
+				if (value == null)
+				{
+					list.RemoveAt(at);
+					UpdateList();
+					return;
+				}
+				list.items[at] = new KeyValuePair<string, string>(tag, value);
+				UpdateList();
 				return;
 			}
 			list.Add(new KeyValuePair<string, string>(tag, value));
-			Update();
+			UpdateList();
 		}
-		public bool add(string tag, string value = null)
+		public bool add(string tag, string value = "")
+		{
+			if (value == null)
+				return false;
+			var at = indexOf(tag);
+			if (at >= 0)
+				return false;
+			list.Add(new KeyValuePair<string, string>(tag, value));
+			UpdateList();
+			return true;
+		}
+		public string at(int at)
 		{
 			if (!parsed)
-				Parse();
-			for (int i = 0; i < list.size; i++)
-			{
-				if (list.items[i].Key.Equals(tag, StringComparison.OrdinalIgnoreCase))
-					return false;
-			}
-			list.Add(new KeyValuePair<string, string>(tag, value));
-			Update();
+				ParseValues();
+			return list[at].Value;
+		}
+		public void at(int at, string value)
+		{
+			if (!parsed)
+				ParseValues();
+			ref var it = ref list.GetRef(at);
+			it = new KeyValuePair<string, string>(it.Key, value);
+		}
+		public bool remove(string tag)
+		{
+			var at = indexOf(tag);
+			if (at < 0)
+				return false;
+			list.RemoveAt(at);
+			UpdateList();
 			return true;
+		}
+		public void removeAt(int at)
+		{
+			if (!parsed)
+				ParseValues();
+			list.RemoveAt(at);
+			UpdateList();
 		}
 	}
 
 	[Description("Tags and values that can be attached to a part (in editor or flight).")]
-	public class PartValues
+	public class PartValues : IDictionary<string, string>, IReadOnlyDictionary<string, string>
 	{
+		//NOTE: this can be null only if ModuleManager is not installed
 		internal readonly RedOnionPartValues values;
+
 		internal PartValues(PartBase part)
 			=> values = part.native.GetComponent<RedOnionPartValues>();
+
+		public override string ToString()
+			=> values?.values ?? "";
+
+		[Description("Get or set the value associated with a key (empty string for tags and 'null' for non-existing / removing).")]
+		public string this[string tag]
+		{
+			get => values?.get(tag) ?? null;
+			set => values?.set(tag, value);
+		}
 
 		[Description("Test for existence of a tag or key-value pair.")]
 		public bool has(string tag)
@@ -208,23 +258,142 @@ namespace RedOnion.KSP.Parts
 		[Description("Test for existence of a tag or key-value pair.")]
 		public bool contains(string tag)
 			=> values?.has(tag) ?? false;
-		[Description("Get the value associated with a key ('null' for tags and non-existing).")]
+		[Description("Get the value associated with a key (empty string for tags, 'null' for non-existing).")]
 		public string get(string tag)
 			=> values?.get(tag) ?? null;
-		[Description("Set the value associated with a key (default 'null' for tags).")]
-		public void set(string tag, string value = null)
+		[Description("Set the value associated with a key (default empty string for tags, 'null' for removing).")]
+		public void set(string tag, string value = "")
 			=> values?.set(tag, value);
-		[Description("Add new tag or key-value pair (default 'null' for tags).")]
-		public bool add(string tag, string value = null)
+		[Description("Add new tag or key-value pair (default empty string, `null` will return false immediately).")]
+		public bool add(string tag, string value = "")
 			=> values?.add(tag, value) ?? false;
-		[Description("Get or set the value associated with a key ('null' for tags and non-existing).")]
-		public string this[string tag]
+
+		[Description("Number of tags and key-value pairs.")]
+		public int count
+			=> values?.count ?? 0;
+		int IReadOnlyCollection<KeyValuePair<string, string>>.Count
+			=> values?.count ?? 0;
+		int ICollection<KeyValuePair<string, string>>.Count
+			=> values?.count ?? 0;
+
+		[Description("Get or set the value associated with a key (empty string for tags and 'null' for non-existing / removing).")]
+		public string this[int at]
 		{
-			get => values?.get(tag) ?? null;
-			set => values?.set(tag, value);
+			get => values?.at(at) ?? null;
+			set => values?.at(at, value);
 		}
 
-		public override string ToString()
-			=> values?.values ?? "";
+		string IReadOnlyDictionary<string, string>.this[string key]
+			=> this[key];
+		string IDictionary<string, string>.this[string key]
+		{
+			get => this[key];
+			set => this[key] = value;
+		}
+
+		public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+		{
+			var values = this.values;
+			if (values == null)
+				yield break;
+			if (!values.parsed)
+				values.ParseValues();
+			foreach (var pair in values.list)
+				yield return pair;
+		}
+		IEnumerator IEnumerable.GetEnumerator()
+			=> GetEnumerator();
+		IEnumerable<string> IReadOnlyDictionary<string, string>.Keys
+		{
+			get
+			{
+				var values = this.values;
+				if (values == null)
+					yield break;
+				if (!values.parsed)
+					values.ParseValues();
+				foreach (var pair in values.list)
+					yield return pair.Key;
+			}
+		}
+		IEnumerable<string> IReadOnlyDictionary<string, string>.Values
+		{
+			get
+			{
+				var values = this.values;
+				if (values == null)
+					yield break;
+				if (!values.parsed)
+					values.ParseValues();
+				foreach (var pair in values.list)
+					yield return pair.Value;
+			}
+		}
+
+		bool IReadOnlyDictionary<string, string>.ContainsKey(string key)
+			=> has(key);
+		bool IReadOnlyDictionary<string, string>.TryGetValue(string key, out string value)
+
+		{
+			var values = this.values;
+			if (values != null)
+			{
+				value = values.get(key);
+				return value != null;
+			}
+			value = null;
+			return false;
+		}
+		bool IDictionary<string, string>.ContainsKey(string key)
+			=> values?.has(key) ?? false;
+		void IDictionary<string, string>.Add(string key, string value)
+			=> values?.add(key, value);
+		bool IDictionary<string, string>.Remove(string key)
+			=> values?.remove(key) ?? false;
+		bool IDictionary<string, string>.TryGetValue(string key, out string value)
+		{
+			var values = this.values;
+			if (values != null)
+			{
+				value = values.get(key);
+				return value != null;
+			}
+			value = null;
+			return false;
+		}
+		void ICollection<KeyValuePair<string, string>>.Add(KeyValuePair<string, string> item)
+			=> values?.add(item.Key, item.Value);
+		void ICollection<KeyValuePair<string, string>>.Clear()
+			=> values?.SetValues("");
+		bool ICollection<KeyValuePair<string, string>>.Contains(KeyValuePair<string, string> item)
+			=> values?.get(item.Key) == item.Value;
+		void ICollection<KeyValuePair<string, string>>.CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+		{
+			var values = this.values;
+			if (values == null)
+				return;
+			if (!values.parsed)
+				values.ParseValues();
+			values.list.CopyTo(array, arrayIndex);
+		}
+		bool ICollection<KeyValuePair<string, string>>.Remove(KeyValuePair<string, string> item)
+		{
+			var values = this.values;
+			if (values == null)
+				return false;
+			if (!values.parsed)
+				values.ParseValues();
+			int at = values.indexOf(item.Key);
+			if (at < 0)
+				return false;
+			if (values.list.items[at].Value != item.Value)
+				return false;
+			values.list.RemoveAt(at);
+			return true;
+		}
+
+		bool ICollection<KeyValuePair<string, string>>.IsReadOnly => false;
+		ICollection<string> IDictionary<string, string>.Keys => throw new NotImplementedException();
+		ICollection<string> IDictionary<string, string>.Values => throw new NotImplementedException();
 	}
 }
--- RedOnion.KSP/Parts/PartValues.md
+++ RedOnion.KSP/Parts/PartValues.md
@@ -4,7 +4,9 @@ Tags and values that can be attached to a part (in editor or flight).
 
 
 **Instance Properties:**
-- `[tag string]`: string - Get or set the value associated with a key ('null' for tags and non-existing).
+- `[tag string]`: string - Get or set the value associated with a key (empty string for tags and 'null' for non-existing / removing).
+- `count`: int - Number of tags and key-value pairs.
+- `[at int]`: string - Get or set the value associated with a key (empty string for tags and 'null' for non-existing / removing).
 
 **Instance Methods:**
 - `has()`: bool, tag string
@@ -12,8 +14,8 @@ Tags and values that can be attached to a part (in editor or flight).
 - `contains()`: bool, tag string
   - Test for existence of a tag or key-value pair.
 - `get()`: string, tag string
-  - Get the value associated with a key ('null' for tags and non-existing).
+  - Get the value associated with a key (empty string for tags, 'null' for non-existing).
 - `set()`: void, tag string, value string
-  - Set the value associated with a key (default 'null' for tags).
+  - Set the value associated with a key (default empty string for tags, 'null' for removing).
 - `add()`: bool, tag string, value string
-  - Add new tag or key-value pair (default 'null' for tags).
+  - Add new tag or key-value pair (default empty string, `null` will return false immediately).
