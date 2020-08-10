c2689887f074671f6bd6de26ec3cef5d62ab3b16
evandisoft <evan_dickinson@protonmail.com>
2020-08-09 17:52:18 -0700

    Basic working version after restructuring.
    Not well tested and bug involving use of ui via lua already identified.

--- RedOnion.KSP/Kerbalua/KerbaluaManager.cs
+++ RedOnion.KSP/Kerbalua/KerbaluaManager.cs
@@ -6,11 +6,59 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using Kerbalua.Scripting;
+using MoonSharp.Interpreter;
+using RedOnion.KSP.Kerbalua.Events;
+using RedOnion.KSP.API;
+using RedOnion.UI;
+using MoonSharp.Interpreter.Interop;
 
 namespace RedOnion.KSP.Kerbalua
 {
 	public class KerbaluaManager : ScriptManager
 	{
+
+		static KerbaluaManager()
+		{
+			Script.GlobalOptions.CustomConverters
+				.SetClrToScriptCustomConversion(
+					(Script script, Vector3d vector3d)
+						=> DynValue.FromObject(script, new Vector(vector3d)) //DynValue.NewTable(new ModuleControlSurfaceProxyTable(this, m))
+					);
+			UserData.RegisterType<Button>(new LuaDescriptor(typeof(Button)));
+
+			UserData.RegistrationPolicy = InteropRegistrationPolicy.Automatic;
+
+			Script.GlobalOptions.CustomConverters
+				.SetScriptToClrCustomConversion(DataType.Function, typeof(Action<Button>), (f) =>
+				{
+					return new Action<Button>((button) =>
+					{
+						var closure=f.Function;
+						var kerbaluaScript=closure.OwnerScript as KerbaluaScript;
+						if (kerbaluaScript==null)
+						{
+							throw new Exception("Ownerscript was not KerbaluaScript in KerbaluaStaticInit");
+						}
+
+						var process=kerbaluaScript?.kerbaluaProcess;
+						if (process==null)
+						{
+							throw new Exception("Could not get current process in KerbaluaStaticInit");
+						}
+
+						var kerbaluaProcess=process as KerbaluaProcess;
+
+						// I don't expect this to ever be the case.
+						if (kerbaluaProcess==null)
+						{
+							throw new Exception("Process in LuaEventDescriptor was not a KerbaluaStaticInit");
+						}
+
+						new KerbaluaThread(kerbaluaProcess, MunPriority.Callback, closure);
+					});
+				});
+		}
+
 		public override string Extension => ".lua";
 
 		public override MunProcess CreateProcess() 
